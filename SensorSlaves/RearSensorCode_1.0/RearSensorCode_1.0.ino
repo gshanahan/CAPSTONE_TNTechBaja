cddbe2f8bcc6a16e3ebb3ae33d596abfe1365ab0
Gabriel Q. Shanahan <41175126+gshanahan@users.noreply.github.com>
2022-09-14 12:01:56 -0500

    Update RearSensorCode_1.0.ino
    Given to team. Updated for front/back sensors, fixed baud rate issue.

--- SensorSlaves/RearSensorCode_1.0/RearSensorCode_1.0.ino
+++ SensorSlaves/RearSensorCode_1.0/RearSensorCode_1.0.ino
@@ -1,47 +1,57 @@
 /*
- * File:    RearSensorCode_1.0.ino
+ * File:    RearSensorCode_1.1.ino
  * Date:    9/13/2022
  * Org:     Software Engineering Baja Team
- * Author:  Gabriel Shanahan
- * Purpose: Reads sensor data for rear half of Baja Buggy (shock travel & wheel speed)
+ * Author:  Gabriel Shanahan / Logan Gillum / Paul Haines / ADD NAME HERE IF EDITED 
+ * Purpose: Reads sensor data for Baja Buggy (shock travel & wheel speed)
  *          and sends to MasterController for data collection and organization.
  */
+
 //SHOCK
-#define potRearDriv A0
-#define potRearPass A1
+#define driverShock A0
+#define passengerShock A1
+
 //HALL EFFECT
-#define hallRearDriv A2
-#define hallRearPass A3
+#define driverHall A2
+#define passengerHall A3
 
-//sensor input setup
 void setup() {
-  Serial.begin(57600);
-  pinMode(potRearDriv, INPUT );
-  pinMode(potRearPass, INPUT );
+  //Setup Function that automatically runs when Arduino is powered on. 
 
-  pinMode(hallRearDriv,INPUT);
-  pinMode(hallRearPass,INPUT);
+  Serial.begin(9600);
+
+  //Define Pins for Shock Inputs
+    pinMode(driverShock, INPUT );
+    pinMode(passengerShock, INPUT );
+
+  //Define Pins for Hall Effect Inputs
+    pinMode(driverHall,INPUT);
+    pinMode(passengerHall,INPUT);
 }
 
-//main sensor input loop
+
 void loop() {
+  //Loop Function for constantly polling sensors while Ardiuno runs 
+
   //SHOCK
-  //REAR driver-side shock sensor
-  int VALpotRearDriv = analogRead(potRearDriv);
-  Serial.print("potRearDriv: "); //print for debug only
-  Serial.println(VALpotRearDriv);
-  //REAR passenger-side shock sensor
-  int VALpotRearPass = analogRead(potRearPass);
-  Serial.print("potRearPass: "); //print for debug only
-  Serial.println(VALpotRearPass);
+    //REAR driver-side shock sensor
+      int VALdriverShock = analogRead(driverShock);
+      Serial.print("driverShock: "); //print for debug only
+      Serial.println(VALdriverShock);
+
+    //REAR passenger-side shock sensor
+    int VALpassengerShock = analogRead(passengerShock);
+    Serial.print("passengerShock: "); //print for debug only
+    Serial.println(VALpassengerShock);
 
   //HALL EFFECT
-  //REAR driver-side hall effect sensor
-  int VALhallRearDriv = analogRead(hallRearDriv);
-  Serial.print("hallRearDriv: "); //print for debug only
-  Serial.println(VALhallRearDriv);
-  //REAR passenger-side hall effect sensor
-  int VALhallRearPass = analogRead(hallRearPass);
-  Serial.print("hallRearPass: "); //print for debug only
-  Serial.println(VALhallRearPass);
+    //REAR driver-side hall effect sensor
+      int VALdriverHall = digitalRead(driverHall);
+      Serial.print("driverHall: "); //print for debug only
+      Serial.println(VALdriverHall);
+
+    //REAR passenger-side hall effect sensor
+      int VALpassengerHall = digitalRead(passengerHall);
+      Serial.print("passengerHall: "); //print for debug only
+      Serial.println(VALpassengerHall);
 }
